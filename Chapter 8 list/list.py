367ad4439b735f7acaa4e9bd2b64c6dd63f92ff1
mofresh27 <81204352+mofresh27@users.noreply.github.com>
2021-07-30 20:44:45 -0700

    Adding txt file 2

---
+++ Chapter 8 list/list.py
@@ -0,0 +1,24 @@
+# favorite_sports_teams=['PSG','FSU','LA Galaxy','LA Clippers','LA Rams']
+# travel = ['Italy','Portugal','England']
+# cars = ['F-pace','X5','Telluride','X3']
+
+# favorite_sports_teams[1]="LA Clippers"
+
+
+
+#print(favorite_sports_teams,travel,cars)
+#print(favorite_sports_teams[0])
+#print(favorite_sports_teams)
+
+
+# fruit = "Banana" traceback error 
+# fruit[0]='b'
+# print(fruit)
+
+# data = "Hello Sally"
+# print(len(data))
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(len(c))
