08dd47ced6e17c6d7c9ae9b3fc0ae1229dce5609
mofresh27 <81204352+mofresh27@users.noreply.github.com>
2021-08-11 12:06:25 -0700

    updated append list v.1

--- Chapter8/romeo.py
+++ Chapter8/romeo.py
@@ -1,5 +1,41 @@
+
+
+#play.clear()
+
+
 FileName = input ('Enter file name here:\n')
-f = open(FileName,'r')
-print(f.read())
+# f = open(FileName,'r')
+fhand = open(FileName) #conditional code may not need 
+play =list()
+
+for word in fhand:
+    #conditional code may not need 
+    word = (word.strip()) #conditional code may not need
+    if word in play: continue
+    play.append(word)
+    play = word.split()
+    print(play)
+
+    
+    #print(play[6])
+    
+
+#print(f.read())
+
+
+# this is a four loop to count lines in a docurment
+
+# FileName = input ('Enter file name here:\n')
+# try:
+#     f = open(FileName,'r')
+# except:
+#     print('File cannot be open:', FileName)
+#     quit()
+
+# count = 0 
+# for line in f:
+#     count = count + 1
+
+# print('There were',count, 'lines in this documnet')
 
 
